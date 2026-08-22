f591ecaab0149c419e556421d03dea87475deee6
Chris <chris.krammer@gmail.com>
2026-08-22 16:46:54 +0200

    build: release v2026.08.17

--- custom_components/smart_irrigation/const.py
+++ custom_components/smart_irrigation/const.py
@@ -5,7 +5,7 @@ class SmartIrrigationError(Exception):
     """Exception raised for errors in the Smart Irrigation integration."""
 
 
-VERSION = "v2026.08.16"
+VERSION = "v2026.08.17"
 NAME = "Smart Irrigation"
 MANUFACTURER = "@JustChr"
 
